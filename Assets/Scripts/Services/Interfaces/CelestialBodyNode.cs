f24b718df307cbb2392f43071bf1ac3de5e59c65
Жеребцов Дмитро Вікторович <ipzk241_zhdv@student.ztu.edu.ua>
2025-05-05 18:33:55 +0300

    CelestialBodyNode class

---
+++ Assets/Scripts/Services/Interfaces/CelestialBodyNode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialBodyNode
+{
+    public string Name { get; }
+    public Transform Transform { get; }
+
+    private readonly List<CelestialBodyNode> _children = new List<CelestialBodyNode>();
+
+    public CelestialBodyNode(string name, Transform transform)
+    {
+        Name = name;
+        Transform = transform;
+    }
+
+    public void AddChild(CelestialBodyNode child)
+    {
+        if (child == null || _children.Contains(child))
+            return;
+        _children.Add(child);
+    }
+
+    public void RemoveChild(CelestialBodyNode child)
+    {
+        if (child == null) return;
+        _children.Remove(child);
+    }
+
+    public IReadOnlyList<CelestialBodyNode> GetChildren() => _children;
+}
